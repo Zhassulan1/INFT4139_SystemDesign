8f8cccb37041f561f5185e5ce7717ecb1da4b0f9
Zhassulan Kainazarov <zhkainazarov@gmail.com>
2025-02-02 23:47:28 +0500

    fix

--- Week-2/locustfile.py
+++ Week-2/locustfile.py
@@ -2,12 +2,11 @@ from locust import HttpUser, task, between
 from random import randint 
 
 
-TOKEN = "7184808c5349a7a26aca59d7dc6dc8c97c2754ddce298a11c587ea0cdf4ec9f73e0f15a714e0c8e79ebfbe5771b7d6745e31"
 class User(HttpUser):
     # wait_time = between(1, 5)
     @task
     def get_token(self):
-        global TOKEN
+        TOKEN = "7184808c5349a7a26aca59d7dc6dc8c97c2754ddce298a11c587ea0cdf4ec9f73e0f15a714e0c8e79ebfbe5771b7d6745e31"
         token = self.client.post(
             "/token", 
             json={"user_id": randint(1, 1000)},
